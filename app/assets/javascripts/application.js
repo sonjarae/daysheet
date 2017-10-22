93c84a015431c1fd3c0df626768574510a239aa4
Broach, Sonja <sonja.broach@getbraintree.com>
2017-10-21 20:11:29 -0500

    switching from omniauth to native fb login

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,27 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '816253168553331',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v2.10'
+    });
+    FB.AppEvents.logPageView();   
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+  function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  }
